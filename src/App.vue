eb9a55a56d9ba72680288d23f7182a4ba85db4a7
mnazarani <mnazarani@gmail.com>
2021-05-13 16:17:55 -0500

    make base form components

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="text-xl text-center text-gray-600 p-4">Hello to the template app.</h1>
+  <h2 class="text-center text-lg font-bold text-gray-500">Will work on forms</h2>
+  <hr class="my-2 py-2">
+
+  <router-view></router-view>
+  
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+
+</style>
